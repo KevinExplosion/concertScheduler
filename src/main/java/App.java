c34c3121d4f7f05b6e8d2c3173837c330e62ae06
kevin <mattisonii@yahoo.com>
2016-03-04 15:52:48 -0800

    Change links on venue.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -61,5 +61,26 @@ public class App {
       response.redirect("/band/" + bandId);
       return null;
     });
+
+    get("/venue/:id", (request, response) -> {
+      HashMap model = new HashMap();
+      int id = Integer.parseInt(request.params("id"));
+      Venue venue = Venue.find(id);
+      model.put("venue", venue);
+      model.put("allBands", Band.all());
+      model.put("template", "templates/venue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venue/:id", (request, response) -> {
+      HashMap model = new HashMap();
+      int venueId = Integer.parseInt(request.queryParams("venueId"));
+      int bandId = Integer.parseInt(request.queryParams("bandName"));
+      Band band = Band.find(bandId);
+      Venue venue = Venue.find(venueId);
+      venue.addBand(band);
+      response.redirect("/venue/" + venueId);
+      return null;
+    });
   }
 }
